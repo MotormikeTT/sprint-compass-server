bd9c5c6de1f6503c661a005639ba4be652e2ff04
George Moussa <georgemoussa19197@gmail.com>
2021-03-15 16:20:14 -0400

    fix update task

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,97 +1,98 @@
 const dbRtns = require("./dbroutines");
 var ObjectId = require("mongodb").ObjectId;
 const {
-  projectcollection,
-  teamcollection,
-  taskcollection,
+	projectcollection,
+	teamcollection,
+	taskcollection,
 } = require("./config");
 
 const resolvers = {
-  projects: async () => {
-    let db = await dbRtns.getDBInstance();
-    return await dbRtns.findAll(db, projectcollection, {}, {});
-  },
-  addproject: async (args) => {
-    let db = await dbRtns.getDBInstance();
-    let project = {
-      name: args.name,
-      team: args.team,
-      startdate: args.startdate,
-      storypointconversion: args.storypointconversion,
-      totalstorypoints: args.totalstorypoints,
-      totalcost: args.totalcost,
-      hourlyrate: args.hourlyrate,
-    };
-    let results = await dbRtns.addOne(db, projectcollection, project);
-    return results.insertedCount === 1 ? project : null;
-  },
-  updateproject: async (args) => {
-    let db = await dbRtns.getDBInstance();
-    let id = args._id;
-    const project = {
-      name: args.name,
-      team: args.team,
-      startdate: args.startdate,
-      storypointconversion: args.storypointconversion,
-      totalstorypoints: args.totalstorypoints,
-      totalcost: args.totalcost,
-      hourlyrate: args.hourlyrate,
-    };
-    let results = await dbRtns.updateOne(
-      db,
-      projectcollection,
-      { _id: new ObjectId(id) },
-      project
-    );
-    return results.lastErrorObject.updatedExisting
-      ? `user was updated`
-      : `user was not updated`;
-  },
+	projects: async () => {
+		let db = await dbRtns.getDBInstance();
+		return await dbRtns.findAll(db, projectcollection, {}, {});
+	},
+	addproject: async (args) => {
+		let db = await dbRtns.getDBInstance();
+		let project = {
+			name: args.name,
+			team: args.team,
+			startdate: args.startdate,
+			storypointconversion: args.storypointconversion,
+			totalstorypoints: args.totalstorypoints,
+			totalcost: args.totalcost,
+			hourlyrate: args.hourlyrate,
+		};
+		let results = await dbRtns.addOne(db, projectcollection, project);
+		return results.insertedCount === 1 ? project : null;
+	},
+	updateproject: async (args) => {
+		let db = await dbRtns.getDBInstance();
+		let id = args._id;
+		const project = {
+			name: args.name,
+			team: args.team,
+			startdate: args.startdate,
+			storypointconversion: args.storypointconversion,
+			totalstorypoints: args.totalstorypoints,
+			totalcost: args.totalcost,
+			hourlyrate: args.hourlyrate,
+		};
+		let results = await dbRtns.updateOne(
+			db,
+			projectcollection,
+			{ _id: new ObjectId(id) },
+			project
+		);
+		return results.lastErrorObject.updatedExisting
+			? `user was updated`
+			: `user was not updated`;
+	},
 
-  projectbyid: async (args) => {
-    let db = await dbRtns.getDBInstance();
-    return await dbRtns.findOne(db, projectcollection, {
-      _id: new ObjectId(args._id),
-    });
-  },
+	projectbyid: async (args) => {
+		let db = await dbRtns.getDBInstance();
+		return await dbRtns.findOne(db, projectcollection, {
+			_id: new ObjectId(args._id),
+		});
+	},
 
-  tasks: async () => {
-    let db = await dbRtns.getDBInstance();
-    return await dbRtns.findAll(db, taskcollection, {}, {});
-  },
+	tasks: async () => {
+		let db = await dbRtns.getDBInstance();
+		return await dbRtns.findAll(db, taskcollection, {}, {});
+	},
 
-  addtask: async (args) => {
-    let db = await dbRtns.getDBInstance();
-    let task = {
-      name: args.name,
-      description: args.description,
-      costestimate: args.costestimate,
-      relativeestimate: args.relativeestimate,
-      projectname: args.projectname,
-    };
-    let results = await dbRtns.addOne(db, taskcollection, task);
-    return results.insertedCount === 1 ? task : null;
-  },
+	addtask: async (args) => {
+		let db = await dbRtns.getDBInstance();
+		let task = {
+			name: args.name,
+			description: args.description,
+			costestimate: args.costestimate,
+			relativeestimate: args.relativeestimate,
+			projectname: args.projectname,
+		};
+		let results = await dbRtns.addOne(db, taskcollection, task);
+		return results.insertedCount === 1 ? task : null;
+	},
 
-  edittask: async (args) => {
-    let db = await dbRtns.getDBInstance();
+	edittask: async (args) => {
+		let db = await dbRtns.getDBInstance();
 
-    let task = {
-      name: args.name,
-      description: args.description,
-      costestimate: args.costestimate,
-      relativeestimate: args.relativeestimate,
-      projectname: args.projectname,
-    };
+		let task = {
+			name: args.name,
+			description: args.description,
+			costestimate: args.costestimate,
+			relativeestimate: args.relativeestimate,
+			projectname: args.projectname,
+		};
 
-    let results = await dbRtns.updateOne(
-      db,
-      taskcollection,
-      { _id: args.id },
-      { task }
-    );
-
-    return results.insertedCount === 1 ? task : null;
-  },
+		let results = await dbRtns.updateOne(
+			db,
+			taskcollection,
+			{ _id: new ObjectId(args.id) },
+			task
+		);
+		return results.lastErrorObject.updatedExisting
+			? `task was updated`
+			: `task was not updated`;
+	},
 };
 module.exports = { resolvers };
--- src/schema.js
+++ src/schema.js
@@ -15,6 +15,14 @@ type Mutation {
         totalcost: Float,
         hourlyrate: Float) : Project,
 
+    updateproject(_id: ID,
+        name: String,
+        team: String,
+        startdate: String,
+        storypointconversion: Int,
+        totalstorypoints: Int,
+        totalcost: Float,
+        hourlyrate: Float) : Project,
         
     addtask(name: String,
         description: String,
@@ -22,21 +30,14 @@ type Mutation {
         relativeestimate: Float,
         projectname: String) : Task,
 
-    edittask(id: ID
+    updatetask(_id: ID
         name: String,
         description: String,
         costestimate: Float,
         relativeestimate: Float,
         projectname: String) : Task,
-        
-updateproject(_id: ID,name: String,
-    team: String,
-    startdate: String,
-    storypointconversion: Int,
-    totalstorypoints: Int,
-    totalcost: Float,
-    hourlyrate: Float) : Project,
 }
+
 type Result {
     results: String
 }
